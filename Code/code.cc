dd4f3fdec888d51ac118c8560487016ced822f74
Kevin Jin <83667515+kevin21jin@users.noreply.github.com>
2023-05-06 15:15:29 -0400

    Update code.cc

--- Code/code.cc
+++ Code/code.cc
@@ -3,6 +3,6 @@
 #define BIQUADRIS_H
 
 // According Policy 71, the code is unavailable to be shared to public. If you are interested
-// in viewing the source code, feel free to reach out to me at k24jin@uwaterloo.ca.x
+// in viewing the source code, feel free to reach out to me at k24jin@uwaterloo.ca :)
 
 #endif
